1cbbaf44d5bf5e8b2a2d515fe5e68e26bf2e2917
hollyhbrady <hollyhbrady@gmail.com>
2021-06-13 14:14:54 +0100

    CabinCrew Test 4 and 5 passed

--- src/test/java/CabinCrewTest.java
+++ src/test/java/CabinCrewTest.java
@@ -1,5 +1,29 @@
+import jdk.nashorn.internal.ir.FunctionNode;
+import org.junit.Before;
+import org.junit.Test;
+import people.staff.CabinCrew;
+import people.staff.Rank;
+import people.staff.Staff;
+
+import static org.junit.Assert.assertEquals;
+
 public class CabinCrewTest {
 
+    CabinCrew cabinCrew;
+    Staff staff;
 
+    @Before
+    public void before(){
+        cabinCrew = new CabinCrew("Izzy", Rank.ATTENDANT);
+    }
 
+    @Test
+    public void cabinCrewHasName(){
+        assertEquals("Izzy", cabinCrew.getName());
+    }
+    
+    @Test
+    public void cabinCrewHasRank(){
+        assertEquals(Rank.ATTENDANT, cabinCrew.getRank());
+    }
 }
