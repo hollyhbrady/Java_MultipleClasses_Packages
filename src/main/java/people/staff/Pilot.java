e1610ee398566294db28c860e8d8f4045bb6cde5
hollyhbrady <hollyhbrady@gmail.com>
2021-06-13 14:21:19 +0100

    Pilot Test 6, 7 and 8 passed

--- src/main/java/people/staff/Pilot.java
+++ src/main/java/people/staff/Pilot.java
@@ -1,4 +1,19 @@
 package people.staff;
 
-public class Pilot {
+public class Pilot extends Staff{
+
+    private String licenseNo;
+
+    public Pilot(String name, Rank rank, String licenseNo) {
+        super(name, rank);
+        this.licenseNo = licenseNo;
+    }
+
+    public String getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(String licenseNo) {
+        this.licenseNo = licenseNo;
+    }
 }
--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+import people.staff.Pilot;
+import people.staff.Rank;
+
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+
+    Pilot pilot;
+
+    @Before
+    public void before(){
+        pilot = new Pilot("James", Rank.CAPTAIN, "SV12345");
+    }
+
+    @Test
+    public void pilotHasName(){
+        assertEquals("James", pilot.getName());
+    }
+
+    @Test
+    public void pilotHasRank(){
+        assertEquals(Rank.CAPTAIN, pilot.getRank());
+    }
+
+    @Test
+    public void pilotHasLicenseNo(){
+        assertEquals("SV12345", pilot.getLicenseNo());
+    }
 }
